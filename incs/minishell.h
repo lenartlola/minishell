fd2047e5c9ce2a649c9242dc43eddbc215083ad8
1mthe0wl <leetpwnsroot@gmail.com>
2021-12-14 17:24:23 +0100

    Organization

---
+++ incs/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsabir <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 13:35:03 by hsabir            #+#    #+#             */
+/*   Updated: 2021/12/14 13:43:07 by hsabir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef struct s_cmdTable
+{
+	char	c;
+}	t_cmdTable;
+
+#endif
