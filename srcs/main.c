12323fa2208b1e65f61bcd6ee1d21b3398f021fa
Leo Gyger <lgyger@c2r7s3.42lausanne.ch>
2021-12-17 16:23:57 +0100

    ctrl-c prints a new prompt only once

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,12 @@
 /*   By: hsabir <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 13:28:25 by hsabir            #+#    #+#             */
-/*   Updated: 2021/12/17 09:46:35 by 1mthe0wl         ###   ########.fr       */
+/*   Updated: 2021/12/17 16:22:24 by lgyger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
-
+int a = 0;
 void	check_tty()
 {
 //	void(argc);
@@ -26,12 +26,16 @@ void	check_tty()
 void	init_line(t_shell *shell)
 {
 	shell->prompt = init_prompt();
+	signal(SIGQUIT, SIG_IGN);
+	rl_replace_line("",0);
 	shell->cmdline = readline(shell->prompt);
-	if (!(ft_strlen(shell->cmdline)))
+	a++;
+	while (!(ft_strlen(shell->cmdline)))
 	{
 		free(shell->prompt);
 		free(shell->cmdline);
-	}
+		init_line(shell);
+	} 
 }
 
 int	main(int argc, char **argv, char **env)
@@ -42,12 +46,13 @@ int	main(int argc, char **argv, char **env)
 //	void(argc);
 //	void(argv);
 	init_shell();
+	shell.cmdline = NULL;
 	while (1)
 	{
-		signal(SIGQUIT, blocksig);
+		a = 0;
 		signal(SIGINT, blocksig);
 		init_line(&shell);
-		if (!(ft_strncmp(shell.cmdline, "exit", 4)))
+		if (shell.cmdline && !(ft_strncmp(shell.cmdline, "exit", 4)))
 		{
 			free(shell.prompt);
 			free(shell.cmdline);
@@ -55,12 +60,15 @@ int	main(int argc, char **argv, char **env)
 			exit (EXIT_SUCCESS);
 		}
 		// We first take care about the pipes.
-		tokenizing(&shell);
-		parsing(&shell);
+		if (shell.cmdline)
+		{
+			tokenizing(&shell);
+			parsing(&shell);
 		//if (!(parsing(&shell)))
 		//		continue ;
-		if (shell.n_pipes)
-			exec_pipe_cmd(&shell, env);
+			if (shell.n_pipes)
+				exec_pipe_cmd(&shell, env);
+		}
 	}
 	return (0);
 }
@@ -69,18 +77,15 @@ void	blocksig(int sig)
 {
 	struct termios termios_new;
 	int rc;
-
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+	
 	rc = tcgetattr(1, &termios_new);
 	termios_new.c_lflag &= ~ECHOCTL;
 	rc = tcsetattr(1, 0, &termios_new );
-	if (sig == SIGINT)
+	if (a == 0)		
 	{
 		printf("\n");
 		rl_on_new_line();
-	}
-		rl_replace_line("", 0);
+		rl_replace_line("",0);
 		rl_redisplay();
+	}
 }
